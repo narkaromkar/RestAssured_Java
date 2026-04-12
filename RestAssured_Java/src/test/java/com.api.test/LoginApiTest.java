e2381ea0558b160fb3abbf3497a7e438e92a9f70
narkaromkar <57225033+narkaromkar@users.noreply.github.com>
2026-04-12 10:12:45 +0000

    Add login API test and update pom

---
+++ RestAssured_Java/src/test/java/com.api.test/LoginApiTest.java
@@ -0,0 +1,12 @@
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class LoginApiTest{
+
+    @Test( description ="Verify Login API is working")
+    public void loginTest(){
+
+    }
+}
